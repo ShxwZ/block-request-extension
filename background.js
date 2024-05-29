05758297171103f1c7439deb8c7ea4e134c992b9
Shaw <shawzgames@gmail.com>
2024-05-29 23:25:33 +0200

    styles

--- background.js
+++ background.js
@@ -2,10 +2,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'updateRules') {
     const blockedUrls = message.rules.blockedUrls;
-    const idsToRemove = message.rules.idsToRemove;
-    console.log(blockedUrls)
-    updateRules(blockedUrls,idsToRemove); // Actualiza las reglas cuando se recibe un mensaje del popup
-    sendResponse({ success: true }); // Envía una respuesta al popup si es necesario
+    console.log('Updating rules:', blockedUrls)
+    updateRules(blockedUrls);
+    sendResponse({ success: true });
   }
 });
 async function getExistingRules() {
@@ -20,7 +19,11 @@ async function getExistingRules() {
   });
 }
 // Esta función actualiza las reglas de bloqueo en la API declarativeNetRequest
-async function updateRules(blockedUrls,idsToRemove = []) {
+async function updateRules(blockedUrls) {
+  if (!blockedUrls) {
+    console.error('blockedUrls is undefined');
+    return;
+  }
   const rules = blockedUrls.map((url, index) => ({
     id: index + 1, // Empezamos desde 1 y seguimos incrementando
     priority: 1,
@@ -31,28 +34,30 @@ async function updateRules(blockedUrls,idsToRemove = []) {
       urlFilter: url
     }
   }));
-  const actualRules = await getExistingRules();
-  console.log("Id to remove",idsToRemove)
-  console.log("actual rules", actualRules)
-  const idToRemoveFinal = actualRules.filter(rule => !rules.includes(r => r.id === rule.id)).map(rule => rule.id)
-  console.log("Id to remove final",idToRemoveFinal)
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: idToRemoveFinal , // Elimina todas las reglas existentes
-    addRules: rules
-  }, () => {
-    console.log(rules)
-    if (chrome.runtime.lastError) {
-      console.error("Error updating rules:", chrome.runtime.lastError.message);
-    } else {
-      console.log("Rules updated successfully.");
-    }
+  await getExistingRules().then(actualRules => {
+
+    const idToRemove = actualRules.filter(rule => !rules.includes(r => r.id === rule.id)).map(rule => rule.id)
+    console.log("Removing rules:", idToRemove);
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: idToRemove, // Elimina todas las reglas existentes
+      addRules: rules
+    }, () => {
+      if (chrome.runtime.lastError) {
+        console.error("Error updating rules:", chrome.runtime.lastError.message);
+      } else {
+        console.log("Rules updated successfully.");
+      }
+    });
+
+
   });
+
+  
 }
 
-// Este oyente se activa cuando se instala o actualiza la extensión
 chrome.runtime.onInstalled.addListener(async () => {
   const blockedUrls = await getBlockedUrls();
-  await updateRules(blockedUrls); // Actualiza las reglas cuando se instala o actualiza la extensión
+  await updateRules(blockedUrls);
 });
 
 // Esta función obtiene las URLs bloqueadas del almacenamiento local
--- popup.js
+++ popup.js
@@ -3,10 +3,8 @@ document.addEventListener('DOMContentLoaded', async () => {
   const urlInput = document.getElementById('urlInput');
   const blockedUrlsList = document.getElementById('blockedUrlsList');
 
-  // Carga las URLs bloqueadas en el popup
   await loadBlockedUrls();
 
-  // Maneja el formulario para agregar una nueva URL bloqueada
   addUrlForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const url = urlInput.value.trim();
@@ -14,53 +12,66 @@ document.addEventListener('DOMContentLoaded', async () => {
       let blockedUrls = await getBlockedUrls();
       blockedUrls.push(url);
       await chrome.storage.local.set({ blockedUrls });
-      await updateRules(blockedUrls); // Actualiza las reglas en el fondo
+      await updateRules(blockedUrls);
       urlInput.value = '';
-      await loadBlockedUrls(); // Vuelve a cargar las URLs bloqueadas en el popup
+      await loadBlockedUrls();
     }
   });
 
-  // Carga y muestra las URLs bloqueadas en el popup
   async function loadBlockedUrls() {
     blockedUrlsList.innerHTML = '';
     const blockedUrls = await getBlockedUrls();
     blockedUrls.forEach((url, index) => {
       const li = document.createElement('li');
-      li.textContent = url;
-      const removeButton = document.createElement('button');
-      removeButton.textContent = 'Remove';
-      removeButton.addEventListener('click', async () => {
+      li.classList.add('blocked-url');
+      
+      const link = document.createElement('a');
+      link.href = url;
+      link.target = "_blank";
+      link.classList.add('url-link');
+      link.textContent = url;
+      const trashButton = document.createElement('div');
+      trashButton.setAttribute('data-tooltip', 'Click to remove');
+      trashButton.classList.add('remove-button');
+      const trashIcon = document.createElement('i');
+      trashIcon.classList.add('fas', 'fa-trash-alt', 'remove-icon');
+      
+                    
+
+      trashButton.addEventListener('click', async () => {
         const url = blockedUrls[index];
-        await removeUrlAndUpdateRules(url); // Elimina la URL y actualiza las reglas
-        await loadBlockedUrls(); // Vuelve a cargar las URLs bloqueadas en el popup
+        await removeUrlAndUpdateRules(url);
+        await loadBlockedUrls();
       });
-      li.appendChild(removeButton);
+      
+      
+      trashButton.appendChild(trashIcon);
+      li.appendChild(link);
+      li.appendChild(trashButton);
       blockedUrlsList.appendChild(li);
     });
   }
+  
 
-  // Obtiene las URLs bloqueadas del almacenamiento local
   async function getBlockedUrls() {
     const result = await chrome.storage.local.get('blockedUrls');
     return result.blockedUrls || [];
   }
 
-  // Esta función elimina una URL del almacenamiento local y actualiza las reglas en consecuencia
   async function removeUrlAndUpdateRules(url) {
     let blockedUrls = await getBlockedUrls();
-    const idsToRemove = blockedUrls.indexOf(url) + 1;
     const index = blockedUrls.indexOf(url);
     if (index !== -1) {
       blockedUrls.splice(index, 1);
       await chrome.storage.local.set({ blockedUrls });
-      await updateRules(blockedUrls,idsToRemove); // Actualiza las reglas en el fondo
+      await updateRules(blockedUrls); 
     }
   }
 
-  // Esta función envía un mensaje al fondo para actualizar las reglas
-  async function updateRules(blockedUrls, idsToRemove = []) {
-    chrome.runtime.sendMessage({ type: 'updateRules', rules: {blockedUrls,idsToRemove} }, response => {
-      console.log(response); // Manejar la respuesta del fondo si es necesario
+
+  async function updateRules(blockedUrls) {
+    chrome.runtime.sendMessage({ type: 'updateRules', rules: {blockedUrls} }, response => {
+      console.log('Rules updated:', response.success);
     });
   }
 });
